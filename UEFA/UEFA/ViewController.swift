6fe95a1995e7e098029f049399b2102edbade917
Ovidiu Pop <ovidiu.pop@endava.com>
2022-02-25 15:46:27 +0200

    Added the initial project

---
+++ UEFA/UEFA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UEFA
+//
+//  Created by Ovidiu Pop on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
